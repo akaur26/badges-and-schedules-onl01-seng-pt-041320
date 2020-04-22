7eb0f3d052475bad6efaeb2d0f0c59a6303f82ff
Anmol Kaur <kaur.anmol26@gmail.com>
2020-04-22 22:38:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,21 +2,3 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end 
 
-def assign_rooms(speakers)
-  assign=[]
-  
-  speakers.each_with_index{|speaker,index|
-  assign.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")
-    }
-   return assign
-end 
-
-def printer(attendees)
-  batch_badge_creator(attendees).each do |badge|
-    puts badge
-  end
-  
-  assign_rooms(attendees).each do |badge|
-    puts badge
-  end
-end 
